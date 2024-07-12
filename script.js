c26354c634a4ef74a51c0c2ac742eaaba998f7d9
SaiNarendra079 <158446698+SaiNarendra079@users.noreply.github.com>
2024-07-12 12:30:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+//Data Types - string , boolean,null,undefined,number
+
+let a=5
+console.log(a,typeof a)
+let b='sai'
+let c= a+b
+console.log(b,typeof b)
+console.log(c, typeof c)
+let x= 1==1
+console.log(x,typeof x)
+let y
+
+console.log(y,typeof y)
+let z= null
+console.log(z,typeof z)
+
+
+//mathematical Operators(basic arthematic operations)
+let A=3
+let B=25
+console.log( 'Addition',A+B)
+console.log("Subtraction:",A-B)
+console.log("Multiplication:",a*B)
+console.log("Modular:",B%A)
+console.log("Division:",A/B)
+
+//Comparator operators
+
+let K=1
+let L='1'
+let M= '2'
+let N=1
+let P=2
+console.log(K==L)
+console.log(K==N)
+console.log(K==M)
+console.log(K===L)
+console.log(K===M)
+console.log(K===N)
+console.log(K!=L)
+console.log(K!=N)
+console.log(K!==L)
+console.log(K!==N)
+console.log(K>N)
+console.log(K<P)
+console.log(K>=N)
+console.log(K<=P)
